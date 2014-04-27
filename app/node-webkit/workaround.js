cbf60dab767aba3e914c1953cd4f1770445d067d
azu <azuciao@gmail.com>
2014-04-27 18:23:49 +0900

    fix(workaround): add workaroung methods.
    vue.js work!

--- app/node-webkit/workaround.js
+++ app/node-webkit/workaround.js
@@ -6,3 +6,5 @@
 global.document = window.document;
 global.navigator = window.navigator;
 window.setTimeout = global.setTimeout;
+window.requestAnimationFrame = global.requestAnimationFrame;
+window.webkitRequestAnimationFrame = global.webkitRequestAnimationFrame;
