8bc643a09f2c6aa05ce68f064a5547508d3c0913
azu <azuciao@gmail.com>
2014-04-29 20:30:43 +0900

    add script

---
+++ _travis/deploy-gh-pages.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+
+if [ $TRAVIS_PULL_REQUEST != 'false' ]; then
+    echo "This is a pull request. No deployment will be done.";
+    exit 0;
+fi
+
+git checkout -B gh-pages
+
+lastCommit=$(git log --oneline | head -n 1)
+echo "=COMMIT="
+echo "MESSAGE :" $lastCommit
+
+git config --global user.email "travis@travis-ci.org"
+git config --global user.name "travis-ci"
+git add -A .
+git commit -m "Travis build $TRAVIS_BUILD_NUMBER"
+git push --force --quiet "https://${GH_TOKEN}@${GH_REF}" gh-pages > /dev/null
