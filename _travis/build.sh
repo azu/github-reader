1985a88b7223ea2bb1a8e378eacdb6f782837ebe
azu <azuciao@gmail.com>
2014-08-24 02:50:43 +0900

    chore(script): change build step
    binary size > 50MB ... then reach GitHub Limit

--- _travis/build.sh
+++ _travis/build.sh
@@ -3,7 +3,7 @@ CURRENT=$(cd $(dirname $0) && pwd)
 
 if [[ "$TRAVIS_TAG" ]]; then
     echo "Release: $TRAVIS_TAG";
-    nwbuild -p "win,osx,linux32,linux64" ./ -o build/
+    zip -q -r ${PWD##*/}.nw *
     sh ${CURRENT}/deploy-gh-pages.sh
 else
     echo "Not Release"
