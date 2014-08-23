e1cc3e9f228de0d7c2c11e1fa15029697e76d299
azu <azuciao@gmail.com>
2014-08-24 02:38:57 +0900

    feat(travis): add release build task

--- _travis/build.sh
+++ _travis/build.sh
@@ -1,2 +1,10 @@
 #!/bin/sh
-zip -q -r ${PWD##*/}.nw *
+CURRENT=$(cd $(dirname $0) && pwd)
+
+if [[ "$TRAVIS_TAG" ]]; then
+    echo "Release: $TRAVIS_TAG";
+    nwbuild -p "win,osx,linux32,linux64" ./ -o build/
+    sh ${CURRENT}/deploy-gh-pages.sh
+else
+    echo "Not Release"
+fi
