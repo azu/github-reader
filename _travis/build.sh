93567fd2346f44005fc6f93f979edcd2e619b658
azu <azuciao@gmail.com>
2014-09-02 09:19:26 +0900

    feat(travis): integrate automatically releasing binary

--- _travis/build.sh
+++ _travis/build.sh
@@ -1,10 +1,16 @@
 #!/bin/bash
-CURRENT=$(cd $(dirname $0) && pwd)
 
 if [[ "$TRAVIS_TAG" ]]; then
-    echo "Release: $TRAVIS_TAG";
-    zip -q -r ${PWD##*/}.nw *
-    sh ${CURRENT}/deploy-gh-pages.sh
+    rm -rf build
+    npm run dist
+    cd build
+    # ziped in build/
+    zip -q github-reader-osx.zip -r github-reader/osx
+    zip -q github-reader-win.zip -r github-reader/win
+    zip -q github-reader-linux32.zip -r github-reader/linux32
+    zip -q github-reader-linux64.zip -r github-reader/linux64
+    cd ../
+    echo "ziped!"
 else
     echo "Not Release"
 fi
